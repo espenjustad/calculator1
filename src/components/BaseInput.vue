a1b168111aab895d0d30a8b31624fbb1ff6a815c
Espen Justad <espenjus@stud.ntnu.no>
2023-02-14 18:47:04 +0100

    working form sending data to db.json

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,26 +1,37 @@
 <template>
-    <label v-if="label">{{ label }}</label>
-    <input
+  <label v-if="label">{{ label }}</label>
+  <input
     v-bind="$attrs"
     :value="modelValue"
     :placeholder="label"
     @input="$emit('update:modelValue', $event.target.value)"
     class="field"
+  />
 
-    >
-  </template>
+  <p v-if="error" class="errorMessage" aria-live="assertive">{{ error }}</p>
+</template>
 
 <script>
 export default {
   props: {
     label: {
       type: String,
-      default: ''
+      default: "",
     },
     modelValue: {
       type: [String, Number],
-      default: ''
+      default: "",
     },
-  }
-}
+    error: {
+      type: String,
+      default: "",
+    },
+  },
+};
 </script>
+
+<style>
+.errorMessage {
+  color: red;
+}
+</style>
